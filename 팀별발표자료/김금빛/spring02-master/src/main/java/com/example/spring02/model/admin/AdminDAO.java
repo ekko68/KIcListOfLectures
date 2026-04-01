80543268d7614e308306b056920e66be02b3ec83
navycui <58658593+ekko68@users.noreply.github.com>
2026-04-01 20:45:28 +0900

    Team presentations
    Team presentations

--- 팀별발표자료/김금빛/spring02-master/src/main/java/com/example/spring02/model/admin/AdminDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.spring02.model.admin;
-
-import com.example.spring02.model.member.dto.MemberVO;
-
-public interface AdminDAO {
-	// 관리자 로그인체크
-	public String loginCheck(MemberVO vo);
-}
--- 팀별발표자료/김금빛/spring02-master/src/main/java/com/example/spring02/service/admin/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.example.spring02.service.admin;
-
-import com.example.spring02.model.member.dto.MemberVO;
-
-public interface AdminService {
-	// 관리자 로그인체크
-	public String loginCheck(MemberVO vo);
-}
